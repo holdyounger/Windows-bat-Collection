2bd083f33b899105b67672e512c8a85f19a7de7c
holdyounger <88082435+holdyounger@users.noreply.github.com>
2025-06-16 09:03:41 +0800

    feat: 修改IP接口跃点数脚本

---
+++ ChangeIfm/ModifyIfm.ps1
@@ -0,0 +1,22 @@
+﻿# Self-elevate the script if required
+# if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+#     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+#         $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+#         Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
+#         Exit
+#  }
+# }
+
+$ChangeIfm = 1
+$WangToChangeIfm = 90
+
+# 获取 InterfaceMetric 为 1 的 -InterfaceIndex
+$NetCard = (Get-NetIPInterface -AddressFamily IPv4 -InterfaceMetric $ChangeIfm).ifAlias
+$netInterfaceIndex = (Get-NetIPInterface -AddressFamily IPv4 -InterfaceMetric $ChangeIfm).ifIndex
+
+Write-Output "检索InterfaceMetric为 [${ChangeIfm}] 列表: `n ${NetCard}"
+Write-Output "Index 为:  ${netInterfaceIndex}"
+
+Set-NetIPInterface -AddressFamily IPv4 -InterfaceIndex $netInterfaceIndex -InterfaceMetric $WangToChangeIfm
+
+Write-Output "已修改 ${NetCard} 的 InterfaceMetric 为 ${WangToChangeIfm}"
---
+++ 修改ip接口的跃点数/GetIfM-1-NetCard.ps1
@@ -0,0 +1,15 @@
+﻿# Self-elevate the script if required
+# if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+#     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+#         $Command = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+#         Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
+#         Exit
+#  }
+# }
+
+# 获取 InterfaceMetric 为 1 的 -InterfaceIndex
+$NetCard = (Get-NetAdapter | Where-Object -FilterScript {$_.ifIndex -eq 10})
+
+Set-NetIPInterface -InterfaceIndex $NetCard.ifIndex -AddressFamily IPv6 -InterfaceMetric 90
+
+Write-Output $NetCard
